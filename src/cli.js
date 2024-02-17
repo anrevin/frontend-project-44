91b841ebb010456c64c01ff331d11c9e1316cdf2
anrevin <anrevin@gmail.com>
2024-02-17 20:59:45 +0300

    сс

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,5 @@ import readlineSync from 'readline-sync';
 
 export default () => {
   const name = readlineSync.question('May I have your name?: ');
-  alert(name);
   return console.log(`Hello, ${name}!`);
 };
