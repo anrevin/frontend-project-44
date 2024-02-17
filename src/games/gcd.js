e777dbeb323cd29663be88bf0f998dd256b33d40
anrevin <anrevin@gmail.com>
2024-02-17 20:50:26 +0300

    сс

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -9,7 +9,6 @@ const round = () => {
   const numberTwo = getRandomNumber(1, 10);
 
   const question = `${numberOne} ${numberTwo}`;
-
   const gcd = () => {
     const devNumberOne = [];
     for (let i = 1; i <= numberOne; i += 1) {
@@ -17,14 +16,12 @@ const round = () => {
         devNumberOne.push(i);
       }
     }
-
     const devNumberTwo = [];
     for (let i = 1; i <= numberTwo; i += 1) {
       if (numberTwo % i === 0) {
         devNumberTwo.push(i);
       }
     }
-
     const commonDev = [];
     for (let i = 0; i < devNumberOne.length; i += 1) {
       for (let j = 0; j < devNumberTwo.length; j += 1) {
@@ -36,10 +33,8 @@ const round = () => {
     const greatestCommonDevision = commonDev[commonDev.length - 1];
     return greatestCommonDevision;
   };
-
   const rightAnswer = gcd();
   const result = [question, rightAnswer.toString()];
-
   return result;
 };
 
